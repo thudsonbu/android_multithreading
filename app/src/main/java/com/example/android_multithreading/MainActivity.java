d1ab795a0e76a015a2e1e6d21b7bd33b979eb27a
hudson_thom@bentley.edu <hudson_thom@bentley.edu>
2021-04-18 15:12:33 -0600

    runnable object

--- app/src/main/java/com/example/android_multithreading/MainActivity.java
+++ app/src/main/java/com/example/android_multithreading/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.android_multithreading;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    Runnable bgThread = new Runnable() {
+
+        public void run() {
+            // wrap in try catch for interrupt exceptions
+            try {
+                // tell the thread to sleep for a second
+                Thread.sleep( 1000 );
+                // generate a random four digit number
+                Random rand = new Random();
+                int randInt = rand.nextInt( ( 9999 - 1000 ) ) + 1000;
+                // write number to log
+                Log.i(
+                "android_multithreading",
+                Thread.currentThread() + " generated " + randInt
+                );
+                // send message to main thread
+                
+
+            } catch ( Exception e ) {
+                e.getMessage();
+            }
+        }
+    };
 }
